1333baa66ba60a9ff593c0d098a1a3478ecb558a
spyros87 <papadopoulos.spyridon@gmail.com>
2019-09-16 20:45:58 +0200

    Bootstrap initial class.

---
+++ src/main/java/com/advent/of/code/day1/ChronalCalibration.java
@@ -0,0 +1,10 @@
+package com.advent.of.code.day1;
+
+public class ChronalCalibration {
+
+    private int frequency;
+
+    public int calibrateFrequency(int... frequencyChanges) {
+        return frequency;
+    }
+}
