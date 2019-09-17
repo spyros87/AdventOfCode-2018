e66f916870d5b1664d8107dfda7a0fc5a76493e4
spyros87 <papadopoulos.spyridon@gmail.com>
2019-09-17 20:39:19 +0200

    ChronalCalibration implementation.

--- src/main/java/com/advent/of/code/day1/ChronalCalibration.java
+++ src/main/java/com/advent/of/code/day1/ChronalCalibration.java
@@ -5,6 +5,9 @@ public class ChronalCalibration {
     private int frequency;
 
     public int calibrateFrequency(int... frequencyChanges) {
+        for (int c : frequencyChanges) {
+            frequency += c;
+        }
         return frequency;
     }
 }
