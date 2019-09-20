7e603fa858d00456e475d241c3ebdc2635dd8c1a
spyros87 <papadopoulos.spyridon@gmail.com>
2019-09-20 23:12:39 +0200

    Add task on boot class.

--- src/main/java/com/advent/of/code/Boot.java
+++ src/main/java/com/advent/of/code/Boot.java
@@ -1,7 +1,22 @@
 package com.advent.of.code;
 
+import com.advent.of.code.day1.ChronalCalibration;
+import com.advent.of.code.utils.Input;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.stream.Stream;
+
 public class Boot {
+    private static final Logger LOG = LoggerFactory.getLogger(Boot.class);
+
     public static void main(String[] args) {
-        System.out.println("Bootstrapping Advent of Code 2018");
+        LOG.info("Bootstrapping Advent of Code 2018");
+
+        Stream<Integer> integerStream = Input.use("day1-part1-input.txt").map(Integer::parseInt);
+        int calibratedFrequency = ChronalCalibration.calibrateFrequency(integerStream);
+        integerStream.close();
+
+        LOG.info("Calibrated Frequency: [{}]", calibratedFrequency);
     }
 }
